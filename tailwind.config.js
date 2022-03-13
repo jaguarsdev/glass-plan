a9c0e57fa5796c75ed8ff672d7f463dd16a2c8fb
jaguars.dev <jaguarsdev.com@gmail.com>
2022-03-13 19:55:09 +0330

    v0-sidebar-icon

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.html"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+      },
+      fontFamily: {
+        'yekb': ['yekanbakh','Arial', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
